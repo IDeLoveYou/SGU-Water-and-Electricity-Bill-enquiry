d9e847bc38c18d4682426a8b463cf6dd3b36a82f
IDeLoveYou <2584787445@qq.com>
2022-10-05 20:58:17 +0800

    添加文件

---
+++ src/main/java/cn/rabig/main/MainClass.java
@@ -0,0 +1,133 @@
+package cn.rabig.main;
+
+import cn.hutool.core.util.CharsetUtil;
+import cn.hutool.core.util.StrUtil;
+import cn.hutool.cron.CronUtil;
+import cn.hutool.cron.task.Task;
+import cn.hutool.setting.dialect.Props;
+import cn.rabig.check.SettingCheck;
+import cn.rabig.utils.CommonUtils;
+import cn.rabig.utils.QueryHttpRequest;
+import cn.rabig.utils.SendMess;
+
+import java.math.BigDecimal;
+import java.text.DecimalFormat;
+import java.util.Timer;
+import java.util.TimerTask;
+
+/**
+ * @Author: MoNo
+ * @Date: 2022/8/2 12:15
+ */
+public class MainClass {
+    public static String room;
+    public static String building;
+    public static String adminUrl;
+    public static String userUrl;
+    public static BigDecimal electricityLowMoney;
+    public static BigDecimal waterLowMoney;
+    public static Float waitTime;
+    static boolean feeUpEnable;
+    public static boolean checkUrlEnable;
+    static boolean weekEnable;
+    public static int weekDay;
+    public static String weekDayString;
+    public static String sessionId;
+    public static String version = "4.4";
+
+    static {
+        //初始化配置文件
+        try {
+            @SuppressWarnings("MismatchedQueryAndUpdateOfCollection")//忽略集合props未更新警告
+            Props props = new Props(System.getProperty("user.dir") + System.getProperty("file.separator") + "setting.ini", CharsetUtil.UTF_8);
+            room = props.getProperty("room");
+            building = props.getProperty("building");
+            adminUrl = props.getProperty("adminUrl");
+            userUrl = props.getProperty("userUrl");
+            electricityLowMoney = props.getBigDecimal("electricityLowMoney");
+            waterLowMoney = props.getBigDecimal("waterLowMoney");
+            waitTime = props.getFloat("waitTime");
+            checkUrlEnable = props.getBool("checkUrlEnable");
+            feeUpEnable = props.getBool("feeUpEnable");
+            weekEnable = props.getBool("weekEnable");
+            weekDay = props.getInt("weekDay");
+            sessionId = props.getProperty("sessionId");
+            if (StrUtil.hasBlank(room, building, adminUrl, userUrl, sessionId)) {
+                CommonUtils.error("请按照规定完善配置文件！");
+                CommonUtils.exit();
+            }
+        } catch (Exception e) {
+            CommonUtils.error("请检查配置文件是否存在并按照规定要求填写！");
+            CommonUtils.exit();
+        }
+    }
+
+    public static void main(String[] args) {
+        //启动参数测试脚本
+        SettingCheck.initAndCheck();
+        //初始化周报部分的两个定时任务
+        if (weekEnable) {
+            //每星期一清除一次周费
+            CronUtil.schedule("0 0 0 ? * MON", (Task) () -> {
+                QueryHttpRequest.weekElectricityFee = BigDecimal.valueOf(0);
+                QueryHttpRequest.weekWaterFee = BigDecimal.valueOf(0);
+            });
+            //周费提醒
+            CronUtil.schedule("0 0 8 ? * " + weekDayString, (Task) () -> SendMess.sendMessage("本周电费花费：" + QueryHttpRequest.weekElectricityFee + "元\n水费花费：" + QueryHttpRequest.weekWaterFee + "元"));
+            CronUtil.start();
+        }
+        //创建定时器循环主函数部分
+        new Timer().schedule(new TimerTask() {
+            @Override
+            public void run() {
+                //查询水电费信息
+                QueryHttpRequest.queryFees();
+
+                //低额提醒部分
+                if (QueryHttpRequest.waitFlag) {
+                    String message = "";
+                    if (QueryHttpRequest.newElectricityFee.compareTo(electricityLowMoney) < 0) {
+                        message = "电费剩余：" + QueryHttpRequest.newElectricityFee + "元，电费小于" + electricityLowMoney + "元\n";
+                    }
+                    if (QueryHttpRequest.newWaterFee.compareTo(waterLowMoney) < 0) {
+                        message = message + "水费剩余：" + QueryHttpRequest.newWaterFee + "元，水费小于" + waterLowMoney + "元\n";
+                    }
+                    if (!"".equals(message)) {
+                        message = message + "提示将在" + new DecimalFormat("###.##").format(waitTime) + "小时后再次提醒您，请及时充值\n" +
+                                "若您已充值，请忽略此提示，企业微信更新余额存在延迟";
+                        SendMess.sendMessage(message);
+                        QueryHttpRequest.waitFlag = false;
+                        new Thread(() -> {
+                            CommonUtils.sleep((int) (waitTime * 3600 * 1000));
+                            QueryHttpRequest.waitFlag = true;
+                        }).start();
+                    }
+                }
+
+                //充值提醒部分
+                if (feeUpEnable) {
+                    if (QueryHttpRequest.oldElectricityFee.compareTo(QueryHttpRequest.newElectricityFee) < 0) {
+                        SendMess.sendMessage("电费充值成功，充值金额" + QueryHttpRequest.newElectricityFee.subtract(QueryHttpRequest.oldElectricityFee) + "元，信息可能有延迟");
+                    }
+                    if (QueryHttpRequest.oldWaterFee.compareTo(QueryHttpRequest.newWaterFee) < 0) {
+                        SendMess.sendMessage("水费充值成功，充值金额" + QueryHttpRequest.newWaterFee.subtract(QueryHttpRequest.oldWaterFee) + "元，信息可能有延迟");
+                    }
+                }
+
+                //周报提醒部分
+                if (weekEnable) {
+                    //周费计算部分
+                    if (QueryHttpRequest.oldElectricityFee.compareTo(QueryHttpRequest.newElectricityFee) > 0) {
+                        QueryHttpRequest.weekElectricityFee = QueryHttpRequest.oldElectricityFee.subtract(QueryHttpRequest.newElectricityFee).add(QueryHttpRequest.weekElectricityFee);
+                    }
+                    if (QueryHttpRequest.oldWaterFee.compareTo(QueryHttpRequest.newWaterFee) > 0) {
+                        QueryHttpRequest.weekWaterFee = QueryHttpRequest.oldWaterFee.subtract(QueryHttpRequest.newWaterFee).add(QueryHttpRequest.weekWaterFee);
+                    }
+                }
+
+                //脚本延迟
+                CommonUtils.log("脚本运行中，请置于后台防止被杀进程");
+            }
+        }, 0, 600000);
+    }
+}
---
+++ src/main/java/cn/rabig/utils/SendMess.java
@@ -0,0 +1,68 @@
+package cn.rabig.utils;
+
+import cn.hutool.http.HttpRequest;
+import cn.rabig.main.MainClass;
+
+public class SendMess {
+    public static final String upInfo = "||更新内容(v" + MainClass.version + ")|\n" +
+            """
+                    |-|-|
+                    |1|增加异常捕获，完善掉线重连机制|
+                    |2|新增充值提醒，该功能会有延迟|
+                    |3|删除sessionId保活进程|
+                    |4|优化对象复用，提高性能|
+                    |5|增加日志时间|
+                    |6|改用ini作为配置文件|
+                    |7|增加参数检测功能|
+                    |8|可自定义低于多少额度提醒|
+                    |9|修复参数检测一些bug|
+                    |10|修复息知信息漏发|
+                    > 愿你在韶院发光发亮
+                    """;
+
+    public static final String adminInfo =
+            """
+                    |代号|错误提示|
+                    |-|-|
+                    |0x001|CommonUtils.exit()函数出现错误|
+                    |1x001|SendMess.sendError()息知管理员信息发送失败|
+                    |1x002|SendMess.sendMessage()息知用户信息发送失败|
+                    > 管理员提示
+                    """;
+
+    public static HttpRequest sendError;
+    public static HttpRequest sendMessage;
+
+    static {
+        sendError = HttpRequest.post(MainClass.adminUrl).keepAlive(true).timeout(20000);
+        sendMessage = HttpRequest.post(MainClass.userUrl).keepAlive(true).timeout(20000);
+    }
+
+    public static void sendError(String error) {
+        try {
+            CommonUtils.error(error);
+            CommonUtils.sleep(1000);//间歇发送息知，避免频繁发送提示导致消息漏发
+            String title = error + "\n\n详细错误提示请点击查看";
+            //请求息知
+            sendError.body("title=" + title + "&" +
+                    "content=" + adminInfo).execute();
+        } catch (Exception e) {
+            CommonUtils.error("息知发送失败(代号:1x001)，错误信息：" + e);
+        }
+
+    }
+
+    public static void sendMessage(String message) {
+        try {
+            CommonUtils.log(message);
+            CommonUtils.sleep(1000);//间歇发送息知，避免频繁发送提示导致消息漏发
+            String title = "\n" + message + "\n\nversion: " + MainClass.version + "，详细更新内容请点击查看";
+            //请求息知
+            sendMessage.body("title=" + title + "&" +
+                    "content=" + upInfo).execute();
+        } catch (Exception e) {
+            CommonUtils.error("息知发送失败(代号:1x002)，错误信息：" + e);
+        }
+    }
+
+}
